764a688982876d05bbca22d0f65c8fbc8d51ec1a
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2024-09-25 21:56:03 +0300

    added new files

---
+++ src/main.py
@@ -0,0 +1,81 @@
+import telebot
+from telebot import types
+
+from tokens import bot_token
+from parser import weather_right_now, get_photo
+
+bot = telebot.TeleBot(bot_token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup_reply = types.ReplyKeyboardMarkup()
+    button_weather_right_now = types.InlineKeyboardButton(text="Weather right now", callback_data="Weather right now")
+    button_picture = types.InlineKeyboardButton(text="Beautiful picture of nature", callback_data="Beautiful picture of nature")
+
+    markup_reply.add(button_weather_right_now, button_picture)
+    if message.text in ['/start', 'Hi', 'Hello', 'Привет', 'start', 'hi', 'hello', 'привет']:
+        bot.send_message(message.chat.id, "Hello! What do you want?", reply_markup=markup_reply)
+        bot.register_next_step_handler(message, answer)
+    elif message.text == 'Yes':
+        bot.send_message(message.chat.id, "What else do you want? Choose weather or picture: ", reply_markup=markup_reply)
+        bot.register_next_step_handler(message, answer)
+    elif message.text == "No":
+        markup_reply = types.ReplyKeyboardMarkup()
+        button_start = types.InlineKeyboardButton(text="start",callback_data="start")
+        markup_reply.add(button_start)
+        bot.send_message(message.chat.id, "Bye! See you later ))", reply_markup=markup_reply)
+        bot.register_next_step_handler(message, start)
+
+    else:
+        bot.send_message(message.chat.id, "Sorry, I don't understand what do you mean :(\nPlease, enter your request again: ")
+
+
+@bot.message_handler(content_types=["text"])
+def answer(message):
+    if message.text == "Weather right now":
+        bot.send_message(message.chat.id, "Ok! Enter your city name: ", reply_markup=types.ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, return_weather_right_now)
+    elif message.text == "Beautiful picture of nature":
+        bot.send_message(message.chat.id, "Ok! What you want to see? Text me, please )) ", reply_markup=types.ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, beautiful_picture)
+    else:
+        bot.send_message(message.chat.id, "Sorry, I don't understand what do you mean :(\nPlease, enter your request again: ")
+        bot.register_next_step_handler(message, answer)
+
+
+@bot.message_handler(content_types=["text"])
+def continue_(message):
+    markup_reply = types.ReplyKeyboardMarkup()
+    button_yes = types.InlineKeyboardButton(text="Yes", callback_data="Yes")
+    button_no = types.InlineKeyboardButton(text="No", callback_data="No")
+    markup_reply.add(button_yes, button_no)
+    bot.send_message(message.chat.id, "Do you want to continue?", reply_markup=markup_reply)
+    bot.register_next_step_handler(message, start)
+
+@bot.message_handler(content_types=["text"])
+def return_weather_right_now(message):
+    try:
+        photo, text = weather_right_now(message.text)
+        if len(text) == 0 or len(photo) == 0:
+            bot.send_message(message.chat.id, "I can't find this city :( \nPlease, write the correct name and try again")
+            bot.register_next_step_handler(message, return_weather_right_now)
+
+        else:
+            bot.send_photo(message.chat.id, caption=text, photo=photo)
+            continue_(message)
+    except:
+        bot.send_message(message.chat.id, "Problems with the site :( \nPlease write back later ")
+        continue_(message)
+
+@bot.message_handler(content_types=["text"])
+def beautiful_picture(message):
+    photo = get_photo(message.text)
+    if len(photo) == 0:
+        bot.send_message(message.chat.id, "I can't find this :( \nPlease, write the correct name and try again")
+        bot.register_next_step_handler(message, beautiful_picture)
+    else:
+        bot.send_photo(message.chat.id, caption="Here is your picture! It's so gorgeous!", photo=photo)
+        continue_(message)
+
+bot.infinity_polling(none_stop = True)
---
+++ src/parser.py
@@ -0,0 +1,77 @@
+import requests
+from tokens import photo_token, weather_token
+
+
+def wind_direction(deg):
+    if 0 <= deg <= 23 or 337 <= deg <= 360:
+        return "N"
+    elif 24 <= deg <= 68:
+        return "NE"
+    elif 69 <= deg <= 113:
+        return "E"
+    elif 114 <= deg <= 158:
+        return "SE"
+    elif 159 <= deg <= 203:
+        return "S"
+    elif 204 <= deg <= 248:
+        return "SW"
+    elif 249 <= deg <= 293:
+        return "W"
+    else:
+        return "NW"
+
+
+def get_photo(query):
+    url = "https://api.unsplash.com//photos/random"
+    params = {'client_id': photo_token, 'query': query}
+    response = requests.get(url=url, params=params)
+    if response.ok:
+        return response.json()['urls']['small']
+    else:
+        return ''
+
+
+def weather_json_parse(response):
+
+    if response.ok:
+
+        information = response.json()
+        description = information['weather'][0]['description']  # rain, snow and ect.
+
+        temperature = int(information['main']['temp'])
+        if temperature > 0:
+            temperature = "+" + str(temperature)
+        else:
+            temperature = str(temperature)
+
+        temp_feels_like = int(information['main']['feels_like'])
+        if temp_feels_like > 0:
+            temp_feels_like = "+" + str(temp_feels_like)
+        else:
+            temp_feels_like = str(temp_feels_like)
+
+        pressure = str(int(int(information['main']['pressure']) * 0.7506))
+        humidity = information['main']['humidity']
+        wind_speed = str(int(information['wind']['speed']))
+        wind_dir = wind_direction(int(information['wind']['deg']))
+
+        result = (
+            f"Now in {information['name']} {temperature}, {description}.\nIt feels like {temp_feels_like}.\nAtmospheric pressure is {pressure} mmHg.\n"
+            f"Humidity is {humidity}%.\nWind: {wind_speed} m/s ({wind_dir})")
+
+        return get_photo(description), result
+
+    else:
+        if response.status_code == 404:
+            return '', ''
+        else:
+            return -1
+
+
+def weather_right_now(city):
+    params = {'q': city, 'appid': weather_token, 'units': 'metric'}
+    url_ = "https://api.openweathermap.org/data/2.5/weather"
+    response_ = requests.get(url=url_, params=params)
+    return weather_json_parse(response_)
+
+
